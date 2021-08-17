8d4fd62119a7b60421b848629a2f3608400bb009
Arthur Frey <afrey@Steelcloud.com>
2021-08-17 12:19:29 -0400

    init

---
+++ Notification.Server/Services/NotificationService.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Notification.Common;
+
+namespace Notification.Server
+{
+    public class NotificationService : NotifierRpc.NotifierRpcBase
+    {
+        public override Task<NotificationReply> WriteNotification(NotificationRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new NotificationReply()
+            {
+                Message = $"You said: {request.Content}"
+            });
+        }
+    }
+}
